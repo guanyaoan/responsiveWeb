80826626def034c3c0a6b5a7a5ce4da29c275256
guanyaoan <13686475101@163.com>
2017-02-21 00:30:23 +0800

    responsiveWeb

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        items: 1,
+        autoplayHoverPause: true,
+        autoplay: true,
+        autoplayTimeout: 2000
+    });
+})
